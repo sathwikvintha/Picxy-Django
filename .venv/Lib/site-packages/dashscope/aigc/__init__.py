70f5f0e1cddec9a4ab5d6846c3c02a6872828d7c
Sathwik Vintha <sv364@snu.edu.in>
2025-02-25 12:48:53 +0530

    changes_25

---
+++ .venv/Lib/site-packages/dashscope/aigc/__init__.py
@@ -0,0 +1,15 @@
+from .conversation import Conversation, History, HistoryItem
+from .generation import Generation
+from .image_synthesis import ImageSynthesis
+from .multimodal_conversation import MultiModalConversation
+from .video_synthesis import VideoSynthesis
+
+__all__ = [
+    Generation,
+    Conversation,
+    HistoryItem,
+    History,
+    ImageSynthesis,
+    MultiModalConversation,
+    VideoSynthesis,
+]
